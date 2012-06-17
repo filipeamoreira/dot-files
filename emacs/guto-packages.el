9da4718eea77566c59e86a7ba4663c0f9ce6ec37
Filipe Moreira <code@filipemoreira.com>
2012-06-17 20:46:21 +0200

    Add default set of packages

---
+++ emacs/guto-packages.el
@@ -0,0 +1,15 @@
+;; Default packages
+(defvar guto-default-packages
+  '(switch-window ace-jump-mode auto-complete autopair clojure-mode coffee-mode
+                  full-ack go-mode haml-mode org prelude-c prelude-clojure
+                  prelude-coffee prelude-css prelude-js prelude-ruby prelude-python
+                  prelude-scheme prelude-scss rinari ruby-end ruby-interpolation
+                  ruby-tools sass-mode smart-tab undo-tree))
+
+(defun guto-install-packages ()
+  (message "%s" "Installing default packages")
+  (dolist (p guto-default-packages)
+    (unless (package-installed-p p)
+      (package-install p))))
+
+(guto-install-packages)
