61a5ea5fed1b1340c7feed71fb549b2dedd49b5a
Filipe Moreira <code@filipemoreira.com>
2012-07-03 10:52:12 +0200

    Disabling sbcl because it is conflicting with swank-clojure

--- emacs/guto-slime.el
+++ emacs/guto-slime.el
@@ -1,2 +1,11 @@
-(load (expand-file-name "~/quicklisp/slime-helper.el"))
-(setq inferior-lisp-program "/usr/local/bin/sbcl")
+;; (load (expand-file-name "~/quicklisp/slime-helper.el"))
+;; (setq inferior-lisp-program "/usr/local/bin/sbcl")
+;; (require 'slime)
+;; (slime-setup)
+
+;; syntax highlighting on slime repl
+(add-hook 'slime-repl-mode-hook
+          (defun clojure-mode-slime-font-lock ()
+            (require 'clojure-mode)
+            (let (font-lock-mode)
+              (clojure-mode-font-lock-setup))))
