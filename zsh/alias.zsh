a75cf03e472589231ff0085f26717ac6e7c66772
Filipe Moreira <code@filipemoreira.com>
2012-08-03 12:48:31 +0200

    Zsh configuration

---
+++ zsh/alias.zsh
@@ -0,0 +1,21 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+
+alias emacs="/Applications/Emacs.app/Contents/MacOS/Emacs -nw"
+# alias ec="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -t"
+alias emacsd="/Applications/Emacs.app/Contents/MacOS/Emacs --daemon"
+# alias e="/Applications/Emacs.app/Contents/MacOS/bin/emacsclient -c"
+
+# a few git aliases
+alias gs='git status'
+alias gd='git diff'
+alias gdh='git diff HEAD'
+alias gl='git pull'
+alias gp='git push'
+alias gb='git branch -a -v'
+# alias g='git'
+
+alias b='bundle exec'
