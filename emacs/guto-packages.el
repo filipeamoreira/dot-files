5d2412d5afafa25b44a094e6e39d2fdbfedbb4dc
Filipe Moreira <code@filipemoreira.com>
2013-02-21 23:09:27 +0100

    Disable automatic installation

--- emacs/guto-packages.el
+++ emacs/guto-packages.el
@@ -1,31 +1,31 @@
-(setq package-user-dir
-      (concat
-       default-directory
-       "elpa"))
-(setq package-archives
-      '(("gnu" . "http://elpa.gnu.org/packages/")
-        ;; ("marmalade" . "http://marmalade-repo.org/packages/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")))
-(package-initialize)
-
-
-;; Using marmalade to install old slime version
-;; (add-to-list 'package-archives
-;;              '("marmalade" .
-;;                "http://marmalade-repo.org/packages/"))
+;; (setq package-user-dir
+;;       (concat
+;;        default-directory
+;;        "elpa"))
+;; (setq package-archives
+;;       '(("gnu" . "http://elpa.gnu.org/packages/")
+;;         ;; ("marmalade" . "http://marmalade-repo.org/packages/")
+;;         ("melpa" . "http://melpa.milkbox.net/packages/")))
 ;; (package-initialize)
 
-;; Default packages
-(defvar guto-default-packages
-  '(switch-window ace-jump-mode auto-complete autopair paredit clojure-mode coffee-mode
-                  full-ack go-mode haml-mode org rinari sass-mode smart-tab undo-tree
-                  nrepl prelude-coffee prelude-css prelude-js prelude-ruby
-                  prelude-python prelude-c prelude-clojure prelude-scheme prelude-scss))
 
-(defun guto-install-packages ()
-  (message "%s" "Installing default packages")
-  (dolist (p guto-default-packages)
-    (unless (package-installed-p p)
-      (package-install p))))
+;; ;; Using marmalade to install old slime version
+;; ;; (add-to-list 'package-archives
+;; ;;              '("marmalade" .
+;; ;;                "http://marmalade-repo.org/packages/"))
+;; ;; (package-initialize)
 
-(guto-install-packages)
+;; ;; Default packages
+;; (defvar guto-default-packages
+;;   '(switch-window ace-jump-mode auto-complete autopair paredit clojure-mode coffee-mode
+;;                   full-ack go-mode haml-mode org rinari sass-mode smart-tab undo-tree
+;;                   nrepl prelude-coffee prelude-css prelude-js prelude-ruby
+;;                   prelude-python prelude-c prelude-clojure prelude-scheme prelude-scss))
+
+;; (defun guto-install-packages ()
+;;   (message "%s" "Installing default packages")
+;;   (dolist (p guto-default-packages)
+;;     (unless (package-installed-p p)
+;;       (package-install p))))
+
+;; (guto-install-packages)
