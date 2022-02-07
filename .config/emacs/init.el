bf02880d82bcb60c3d70ab36d558b8502de2ae28
Filipe Moreira <code@filipeamoreira.com>
2022-02-07 09:03:26 +0000

    fix(emacs): configuration for org-roam

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -429,16 +429,24 @@
   (add-to-list 'org-structure-template-alist '("hs" . "src haskell")))
 
 (use-package org-roam
+  :straight t
   :ensure t
-:init
-(setq org-roam-v2-ack t)
   :custom
-  (org-roam-directory "~/sync/notes/org-roam")
+  (org-roam-directory (file-truename "~/sync/notes/org-roam/"))
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
-         ("C-c n i" . org-roam-node-insert))
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
   :config
-  (org-roam-setup))
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (setq org-roam-completion-everywhere t)
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
 
 (use-package lsp-mode
   :ensure t
