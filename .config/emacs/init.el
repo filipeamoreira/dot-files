c771fa9661434655d1601aba88e6fcc71e1e795a
Filipe Moreira <code@filipeamoreira.com>
2022-02-11 07:30:53 +0000

    fix(emacs): improve org-roam configuration

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -433,17 +433,19 @@
   :ensure t
   :custom
   (org-roam-directory (file-truename "~/sync/notes/org-roam/"))
+  (org-roam-completion-everywhere t)
   :bind (("C-c n l" . org-roam-buffer-toggle)
          ("C-c n f" . org-roam-node-find)
          ("C-c n g" . org-roam-graph)
          ("C-c n i" . org-roam-node-insert)
          ("C-c n c" . org-roam-capture)
-         ;; Dailies
-         ("C-c n j" . org-roam-dailies-capture-today))
+         ("C-c n j" . org-roam-dailies-capture-today)
+         :map org-mode-map
+         ("C-M-i"   . completion-at-point))
   :config
   ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
-  (setq org-roam-completion-everywhere t)
+
   (org-roam-db-autosync-mode)
   ;; If using org-roam-protocol
   (require 'org-roam-protocol))
