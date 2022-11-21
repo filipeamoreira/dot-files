567383a078c2af3465e77360b11c9019d48c642a
Filipe Moreira <code@filipeamoreira.com>
2022-11-21 11:05:02 +0000

    add(emacs): Elixir config

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -811,6 +811,45 @@
   :config (setq tab-width 4)
   )
 
+(use-package elixir-mode
+  :ensure t
+  :init
+  (add-hook 'elixir-mode-hook
+            (lambda ()
+              (push '(">=" . ?\u2265) prettify-symbols-alist)
+              (push '("<=" . ?\u2264) prettify-symbols-alist)
+              (push '("!=" . ?\u2260) prettify-symbols-alist)
+              (push '("==" . ?\u2A75) prettify-symbols-alist)
+              (push '("=~" . ?\u2245) prettify-symbols-alist)
+              (push '("<-" . ?\u2190) prettify-symbols-alist)
+              (push '("->" . ?\u2192) prettify-symbols-alist)
+              (push '("<-" . ?\u2190) prettify-symbols-alist)
+              (push '("|>" . ?\u25B7) prettify-symbols-alist))))
+
+(use-package reformatter
+  :ensure t
+  :config
+  ; Adds a reformatter configuration called "+elixir-format"
+  ; This uses "mix format -"
+  (reformatter-define +elixir-format
+    :program "mix"
+    :args '("format" "-"))
+  ; defines a function that looks for the .formatter.exs file used by mix format
+  (defun +set-default-directory-to-mix-project-root (original-fun &rest args)
+    (if-let* ((mix-project-root (and buffer-file-name
+                                     (locate-dominating-file buffer-file-name
+                                                             ".formatter.exs"))))
+        (let ((default-directory mix-project-root))
+          (apply original-fun args))
+      (apply original-fun args)))
+  ; adds an advice to the generated function +elxir-format-region that sets the proper root dir
+  ; mix format needs to be run from the root directory otherwise it wont use the formatter configuration
+  (advice-add '+elixir-format-region :around #'+set-default-directory-to-mix-project-root)
+  ; Adds a hook to the major-mode that will add the generated function +elixir-format-on-save-mode
+  ; So, every time we save an elixir file it will try to find a .formatter.exs and then run mix format from
+  ; that file's directory
+  (add-hook 'elixir-mode-hook #'+elixir-format-on-save-mode))
+
 (use-package company
   :after lsp-mode
   :hook (
