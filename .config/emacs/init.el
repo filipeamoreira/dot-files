0e01c127aa4fe30b803aaf55ae8bfe5dc839edf7
Filipe Moreira <code@filipeamoreira.com>
2023-07-13 13:12:14 +0100

    fix(emacs): set backup directory

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -120,6 +120,14 @@
 
 (modify-all-frames-parameters '((inhibit-double-buffering . t)))
 
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+backup-by-copying t    ; Don't delink hardlinks
+version-control t      ; Use version numbers on backups
+delete-old-versions t  ; Automatically delete excess backups
+kept-new-versions 20   ; how many of the newest versions to keep
+kept-old-versions 5    ; and how many of the old
+)
+
 ;; Call `emojify-apropos-emoji` or `emojify-insert-emoji`
 (use-package emojify
   :hook (after-init . global-emojify-mode))
